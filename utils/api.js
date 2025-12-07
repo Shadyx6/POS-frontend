84ec8b5b67c1caf118c37a71cdc3882bb028122e
Shadyx6 <shadkhanxox@gmail.com>
2025-12-07 18:02:49 +0530

    server error

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-console.log(import.meta.env.VITE_API_URL)
+// console.log(import.meta.env.VITE_API_URL)
 const api = axios.create({
     baseURL: "https://pos-backend-lkuv.onrender.com",
     withCredentials: true
