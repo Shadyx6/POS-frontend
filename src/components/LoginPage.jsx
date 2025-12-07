984a922ea1db804e3cc3ebbecd54b2050d32954a
Shadyx6 <shadkhanxox@gmail.com>
2025-12-07 18:25:38 +0530

    extra logginf

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -25,6 +25,7 @@ function LoginPage() {
             })
             console.log(res)
             if (res.data.success){
+                console.log(res.data)
                 dispatch(Login(res.data.data))
                 
                 navigate("/")
@@ -35,6 +36,7 @@ function LoginPage() {
         } catch (error) {
             toast.error("server error")
             console.log("here")
+            console.log(error)
             setError(error.message)
             return navigate("/server-error")
         }
